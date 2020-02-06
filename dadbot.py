5c0df3a246f820c23b5a9d95c135a3ef587ddec4
Peter Pegues <peter.pegues@gmail.com>
2020-02-06 00:48:16 -0500

    formatting

--- dadbot.py
+++ dadbot.py
@@ -4,30 +4,36 @@ from typing import List, Tuple
 from random import choice
 import re
 
+
 class DadBot(Plugin):
-    @command.passive(r"\bi['’]?m\s+(\w+)", case_insensitive=True) ## return word after im 
+    # return word after im
+    @command.passive(r"\bi['’]?m\s+(\w+)", case_insensitive=True)
     async def handler(self, evt: MessageEvent, match: tuple) -> None:
-        
+        # match[0] whole string, match[1] returned value
         await evt.mark_read()
-        # Match[0] whole string, Match[1] returned value
         self.log.debug(f"dadbot matched {match}")
-        
-        if match: # check for match
 
-            excp_list = ["a", "an", "on", "in", "at" ] # exception matches
-            reg = r"\b%s\s+(\w+)" %(match[1]) # feed returned value into new reg
-            new_match = re.search(reg,match[0], re.IGNORECASE)
-
-            if match[1] in excp_list and new_match: # Exception response
-                    result = (f"Hi {new_match.group()} I'm dad")
-           
-            else:  # Normal respone
+        if match:
+            # exception matches
+            excp_list = ["a", "an", "on", "in", "at"]
+            # feed returned value into new reg
+            reg = r"\b%s\s+(\w+)" % (match[1])
+            new_match = re.search(reg, match[0], re.IGNORECASE)
+            
+            # Exception response
+            if match[1] in excp_list and new_match:
+                result = (f"Hi {new_match.group()} I'm dad")
+            
+            # Normal response
+            else:
                 result = f"Hi {match[1]} I'm Dadbot"
-        else: # Empty match
-            result = f"Hi ERROR I'm Dabdort"
         
+        # Empty match
+        else:
+            result = f"Hi ERROR I'm Dabdort"
+
         await evt.reply(result, allow_html=True)
-    
+
     # Give the user an uplifting message
     @command.new("validation")
     async def validation_handler(self, evt: MessageEvent) -> None:
